dce3c716034dc9fdf70989905854f885e1e3024d
StephanieRuthJones <stephanieruthjones@gmail.com>
2022-10-12 15:34:49 -0600

    Add uncontrolled form and controlled form

--- src/components/ControlledForm.js
+++ src/components/ControlledForm.js
@@ -1,20 +1,35 @@
-import React, { useState } from "react";
-function ControlledForm() {
-  const [value, setValue] = useState("");
-  function handleChange(event) {
-    setValue(event.target.value);
-  }
-  function handleSubmit(event) {
-    alert("A name was submitted: " + value);
-    event.preventDefault();
-  }
+import { useState } from "react";
+import React from "react";
+
+const ControlledForm = () => {
+  //In this controlled component, we will create a state variable for our input
+  const [email, updateEmail] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name: <input type="text" value={value} onChange={handleChange} />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Enter email"
+          //Setting the value of the input to the email value in state is what makes this input controlled
+          //the text this input contains will be the value of its state variable
+          value={email}
+          //Controlled inputs also have an onChange handler that updates state as a user types
+          //This creates a two-way binding between the value of the input and the value of the state variable
+          onChange={(e) => updateEmail(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+      <div>
+        <p>Form Value: {email}</p>
+      </div>
+    </>
   );
-}
+};
+//Controlled components allow you to do:
+//form validation while user is typing
+//
 export default ControlledForm;
--- src/components/UncontrolledForm.js
+++ src/components/UncontrolledForm.js
@@ -1,14 +1,31 @@
 import { useRef } from "react";
 import React from "react";
+// There is no updating or altering of states when the input values are uncontrolled.
+// Uncontrolled components keep track of their internal state, which means the component remembers what you entered into the field.
+//That value can be manipulated by utilizing the ref keyword to fetch it whenever it is needed.
+//The value you submit is the value you get with uncontrolled inputs.
+// Refs allow you to access DOM nodes or React elements formed during the render process.
 
+// Uncontrolled components behave more like HTML form elements.
+//The DOM, not the component, stores the data for each input element.
+//Each input tracks its own state until we click submit - this makes the component uncontrolled.
 export const UncontrolledForm = () => {
   //The useRef Hook allows you to persist values between renders.
   // It can be used to store a mutable value that does not cause a re-render when updated.
   // It can be used to access a DOM element directly.
-  const nameInput = useRef();
+  //useRef returns a mutable ref object whose .current property is initialized to the passed argument ( initialValue ).
+  //The returned object will persist for the full lifetime of the component.
+  //Essentially, useRef is like a “box” that can hold a mutable value in its.current property.
+  //In this example, we have a ref attribute called nameInput
+  //When we initialize useRef we set the initial value: useRef(initial value).
+  const nameInput = useRef(null);
   const handleSubmit = (e) => {
     e.preventDefault();
+    //We can access the value of the nameInput by referencing nameInput.current.value
+    console.log(nameInput);
+    console.log(nameInput.current);
     console.log(nameInput.current.value);
+    console.log(nameInput.current.name);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -20,6 +37,7 @@ export const UncontrolledForm = () => {
         ref={nameInput}
       />
       <input type="submit" value="Submit" />
-    </form>
+      </form>
+      
   );
 };
